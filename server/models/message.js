a50ce0c28ac2698f9c6ce16c3dd1b1daf946d785
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-09-25 13:02:15 +0100

    Define message schema

---
+++ server/models/message.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  username: { type: String, required: true },
+  timestamp: { type: Date, required: true },
+});
+
+module.exports = mongoose.model("Message", messageSchema);
